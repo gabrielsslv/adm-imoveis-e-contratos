ebc0e5c268ab1887019293815cd82a47083ee411
gabriel <gabrielseg.ti@gmail.com>
2025-09-18 09:48:23 -0300

    "pequeno ajuste"

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,27 +1,8 @@
 from django.contrib import admin
 from .models import Cliente, Imovel, Contrato
 
-@admin.register(Cliente)
-class ClienteAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'email', 'telefone')
-    search_fields = ('nome', 'email', 'cnpj')
-    list_filter = ('nome', 'email')
-
-@admin.register(Imovel)
-class ImovelAdmin(admin.ModelAdmin):
-    list_display = ('endereco', 'bairro', 'cidade', 'valor_aluguel', 'disponivel')
-    search_fields = ('endereco', 'bairro', 'cidade', 'valor_aluguel')
-    list_filter = ('disponivel', 'cidade', 'bairro', 'valor_aluguel')
-    ordering = ('cidade', 'valor_aluguel')
-
-@admin.register(Contrato)
-class ContratoAdmin(admin.ModelAdmin):
-    list_display = ('cliente', 'imovel', 'data_inicio', 'data_fim', 'valor_mensal')
-    search_fields = ('cliente__nome', 'imovel__endereco')
-    list_filter = ('data_inicio', 'data_fim')
-
 class ValorAluguelFilter(admin.SimpleListFilter):
-    title = 'Valor do Aluguel'
+    title = 'Faixa de valor do Aluguel'
     parameter_name = 'valor_aluguel'
 
     def lookups(self, request, model_admin):
@@ -39,3 +20,22 @@ class ValorAluguelFilter(admin.SimpleListFilter):
         if self.value() == 'alto':
             return queryset.filter(valor_aluguel__gt=3000)
         return queryset
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'telefone')
+    search_fields = ('nome', 'email', 'cnpj')
+    list_filter = ('nome', 'email')
+
+@admin.register(Imovel)
+class ImovelAdmin(admin.ModelAdmin):
+    list_display = ('endereco', 'bairro', 'cidade', 'valor_aluguel', 'disponivel')
+    search_fields = ('endereco', 'bairro', 'cidade', 'valor_aluguel')
+    list_filter = ('disponivel', 'cidade', 'bairro', ValorAluguelFilter)
+    ordering = ('cidade', 'valor_aluguel')
+
+@admin.register(Contrato)
+class ContratoAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'imovel', 'data_inicio', 'data_fim', 'valor_mensal')
+    search_fields = ('cliente__nome', 'imovel__endereco')
+    list_filter = ('data_inicio', 'data_fim')
